10576123c1f7b50ee8f2f18bdc61b36539b4c0de
Omar <omar.ifpr2024@gmail.com>
2026-03-04 10:06:56 -0300

    Add files via upload

---
+++ Trabalhos/Trabalho1/Botões/model/Link.php
@@ -0,0 +1,37 @@
+<?php
+
+class Link {
+
+    private $linkImg;
+    private $info;
+
+    public function __construct($img, $info)
+    {
+        $this -> linkImg = $img;
+        $this -> info = $info;
+    }
+    
+    public function getLinkImg()
+    {
+        return $this->linkImg;
+    }
+
+    public function setLinkImg($linkImg): self
+    {
+        $this->linkImg = $linkImg;
+
+        return $this;
+    }
+
+    public function getInfo()
+    {
+        return $this->info;
+    }
+
+    public function setInfo($info): self
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+}
