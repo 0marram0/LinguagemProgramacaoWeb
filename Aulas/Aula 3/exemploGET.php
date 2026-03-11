ef85ea5b95504b001ce11477d33a5427e000ec2f
Omar <omar.ifpr2024@gmail.com>
2026-03-11 09:50:48 -0300

    Add files via upload

---
+++ Aulas/Aula 3/exemploGET.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Parâmetros recebidos por GET: ";  
+
+echo "<br>";
+echo $_GET['nome'];
+
+echo "<br>";
+$idade = $_GET['idade'];
+echo "Idade comédia: " . $idade;
+
+?>
---
+++ Aulas/Aula 3/exemploPOST.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Parâmetros recebidos por GET: ";  
+
+echo "<br>";
+echo $_POST['nome'];
+
+echo "<br>";
+$idade = $_POST['idade'];
+echo "Idade comédia: " . $idade;
+
+?>
+
