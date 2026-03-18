6c344d9869dec13d00b0f2c92d061d74ee78ab5c
Omar <omar.ifpr2024@gmail.com>
2026-03-18 08:26:24 -0300

    Add files via upload

--- Exercícios/Exer 02/model/Pessoa.php
+++ Exercícios/Exer 02/model/Pessoa.php
@@ -1,1 +1,46 @@
+<?php
 
+class Pessoa {
+
+    //Atributos
+    private string $nome;
+    private string $sobrenome;
+    private int $idade;
+
+    //GETs e SETs
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getSobrenome(): string
+    {
+        return $this->sobrenome;
+    }
+
+    public function setSobrenome(string $sobrenome): self
+    {
+        $this->sobrenome = $sobrenome;
+
+        return $this;
+    }
+
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+    public function setIdade(int $idade): self
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+}
